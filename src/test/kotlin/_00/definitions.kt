d2419ea55a5d9541ebc4605b4abaa040fef71231
David Denton <denton.david@gmail.com>
2020-04-21 19:28:14 +0100

    adding infra

--- src/test/kotlin/_00/definitions.kt
+++ src/test/kotlin/_00/definitions.kt
@@ -4,3 +4,5 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 
 typealias HttpHandler = (Request) -> Response
+
+interface Filter : (HttpHandler) -> HttpHandler
--- src/test/kotlin/_05/definitions.kt
+++
@@ -1,8 +0,0 @@
-package _05
-
-import org.http4k.core.Request
-import org.http4k.core.Response
-
-typealias HttpHandler = (Request) -> Response
-
-interface Filter : (HttpHandler) -> HttpHandler
