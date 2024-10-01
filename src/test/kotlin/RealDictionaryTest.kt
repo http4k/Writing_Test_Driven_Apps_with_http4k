3de0e6b623d54df02d24e4267496b9f12c2319a3
David Denton <denton.david@gmail.com>
2024-10-01 10:03:14 +0100

    Disable real dictionary test

--- src/test/kotlin/RealDictionaryTest.kt
+++ src/test/kotlin/RealDictionaryTest.kt
@@ -2,7 +2,9 @@ import org.http4k.client.OkHttp
 import org.http4k.core.Uri
 import org.http4k.core.then
 import org.http4k.filter.ClientFilters
+import org.junit.jupiter.api.Disabled
 
+@Disabled
 class RealDictionaryTest : DictionaryContract() {
     override val http = ClientFilters.SetBaseUriFrom(Uri.of("http://api.dictionary.com:10000")).then(OkHttp())
 }
